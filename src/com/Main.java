4e1b37d5745215b29f1a230e0f5f2a22f33416a8
RomanV521 <romav5166@gmail.com>
2022-05-23 11:31:38 +0300

    During 1

--- src/com/Main.java
+++ src/com/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         ComplexNumber complexNumber = new ComplexNumber();
 
-        complexNumber.module();
+        double a = complexNumber.x;
+
     }
 }
